71f38c22606670275b39d92ef9ba4aed510c4104
AliGhaffarian <alighaffarian9@gmail.com>
2026-09-23 16:43:31 +0330

    helper: add GET_SIGN_BIT

--- src/helper/helper.h
+++ src/helper/helper.h
@@ -2,6 +2,8 @@ void freep(void *ptr);
 
 #define _cleanup_free_ __attribute__((__cleanup__(freep)))
 
+#define GET_SIGN_BIT(num) ({ (num) >> ((sizeof(typeof(num)) * 8) - 1); })
+
 #define MOVE(ptr_to_ptr)                                                       \
     ({                                                                         \
         void *__tmp_move_var = *(ptr_to_ptr);                                  \
